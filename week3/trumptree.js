8462427e5e2f166230cebd369dae9eec99869e51
toludwig <toludwig@uos.de>
2017-12-08 16:19:03 +0100

    tree ongoing

--- week3/trumptree.js
+++ week3/trumptree.js
@@ -1,7 +1,34 @@
+
+
 function drawtree(data){
-    d3.select("#plotarea")
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("r", 20);
+    var margin = {top: 20, right: 120, bottom:20, left: 20};
+    var width = 960 - margin.right - margin.left;
+    var height = 500 - margin.top - margin.bottom;
+
+    var tree = d3.tree()
+        .size([height, width]);
+
+    var root = d3.hierarchy(data,
+        function children(d){
+            if(d["children"] != undefined)
+                return d.children;
+            else return d.partners;
+        });
+
+    var svg = d3.select("#plotarea")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", width + margin.top + margin.bottom)
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var link = g.selectAll(".link")
+        .data(tree(root).links())
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", d3.linkHorizontal()
+            .x(function(d) { return d.y; })
+            .y(function(d) { return d.x; }));
+
+    tree.nodes();
+
+}
