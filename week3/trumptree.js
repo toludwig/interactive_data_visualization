de83012a1088e8cfd90a769d97e1146e3565af77
Mona <33896612+monatimmermann@users.noreply.github.com>
2017-12-09 14:45:47 +0100

    Update trumptree.js

--- week3/trumptree.js
+++ week3/trumptree.js
@@ -1,9 +1,21 @@
 
 
+
 function drawtree(data){
     var margin = {top: 20, right: 120, bottom:20, left: 20};
     var width = 960 - margin.right - margin.left;
     var height = 500 - margin.top - margin.bottom;
+
+
+    //this place the text x axis adjust this to center align the text
+    var tx = function (d) {
+        return d.x - 3;
+    };
+    //this place the text y axis adjust this to center align the text
+    var ty = function (d) {
+        return d.y + 3;
+    };
+
 
     var tree = d3.tree()
         .size([height, width]);
@@ -23,21 +35,41 @@ function drawtree(data){
 
     var link = g.selectAll(".link")
         .data(tree(root).links())
-        .enter().append("path")
+        .enter()
+        .append("path")
         // TODO schönere Links
         // TODO alle Links!!
         .attr("class", "link")
         .attr("d",  function link(d) {
                 return "M" + d.source.x + "," + d.source.y
                       + "L" + (d.source.x + d.target.x)/2 + "," + d.source.y
-                      + " " + (d.source.x + d.target.x)/2 + "," + d.target.y
-                      + " " + d.target.x + "," + d.target.y;
+                      // + " " + (d.source.x + d.target.x)/2 + "," + d.target.y
+                       + " " + d.target.x + "," + d.target.y;
                 }
         )
         .attr("fill", "none")
-        .attr("stroke", "black");
+        .attr("stroke", "grey");
 
 
+    var node = g.selectAll(".node")
+        .data(root.descendants())
+        .enter()
+        .append('g')
+        .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    node.append("circle")
+        .attr("r", 15)
+        .style("fill", "white")
+        .style("stroke", "black");
+
+    console.log(data.name);
+
+    node.append("text")
+        // FIXME ich arbeite gerade dran
+        .attr('text-anchor','middle')
+        .text(function(d) { return "test" });
+}
     var node = g.selectAll(".node")
         .data(root.descendants())
         .enter().append("g")
