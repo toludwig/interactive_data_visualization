a931f56452452a8a689d41790d34b2c86184452e
toludwig <toludwig@uos.de>
2017-12-09 21:36:52 +0100

    verschiedene Farben, Text ok

--- week3/trumptree.js
+++ week3/trumptree.js
@@ -1,7 +1,7 @@
 function drawtree(data) {
     var margin = {top: 20, right: 120, bottom: 20, left: 20};
-    var width = 800 - margin.right - margin.left;
-    var height = 800 - margin.top - margin.bottom;
+    var width = 2000 - margin.right - margin.left;
+    var height = 600 - margin.top - margin.bottom;
 
 
     //this place the text x axis adjust this to center align the text
@@ -15,7 +15,7 @@ function drawtree(data) {
 
 
     var tree = d3.tree()
-        .size([height, width]);
+        .size([width, height]);
 
     var root = d3.hierarchy(data,
         function children(d) {
@@ -31,44 +31,49 @@ function drawtree(data) {
     var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
     var link = g.selectAll(".link")
         .data(tree(root).links())
         .enter().append("path")
         .attr("class", "link")
-        .attr("d", function link(d) {
-                return "M" + d.source.x + "," + d.source.y
-                    + "L" + (d.source.x + d.target.x) / 2 + "," + d.source.y
-                    + " " + d.target.x + "," + d.target.y;
+        .attr("d", function (link) {
+            return "M" + link.source.x + "," + link.source.y
+                + "L" + (link.source.x + link.target.x) / 2 + "," + link.source.y
+                + " " + link.target.x + "," + link.target.y;
             }
         )
         .attr("fill", "none")
-        .attr("stroke", "grey");
+        .attr("stroke", function(link) {
+            return (link.source.depth % 2 == 0) ? "red" : "blue";
+        })
+        .attr("stroke-width", function(link) {
+            return (link.source.depth % 2 == 0) ? 4 : 2;
+        });
 
 
     var node = g.selectAll(".node")
         .data(root.descendants())
         .enter()
         .append('g')
-        .attr("class", function (d) {
-            return "node" + (d.children ? " node--internal" : " node--leaf");
+        .attr("class", function (node) {
+            return "node" + (node.children ? " node--internal" : " node--leaf");
         })
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
+        .attr("transform", function (node) {
+            return "translate(" + node.x + "," + node.y + ")";
         });
 
-    node.append("circle")
-        .attr("r", 15)
-        .style("fill", "white")
-        .style("stroke", "black");
-
-    console.log(data.name);
+    node.append("rect")
+        .attr("x", -60)
+        .attr("y", -15)
+        .attr("width", 120)
+        .attr("height", 30)
+        .style("stroke", "black")
+        .style("fill", "white");
 
     node.append("text")
-    // FIXME
         .attr('text-anchor', 'middle')
-        .text(function (d) {
-            return d.data.name;
+        .text(function (node) {
+            return node.data.name;
         });
-
 
 }
