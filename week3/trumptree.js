09cc1839fb86e950e931d354469f087f8b491497
Mona <33896612+monatimmermann@users.noreply.github.com>
2017-12-11 14:06:50 +0100

    final assignment 3 js

--- week3/trumptree.js
+++ week3/trumptree.js
@@ -1,22 +1,13 @@
 function drawtree(data) {
-    var margin = {top: 20, right: 120, bottom: 20, left: 20};
+    var margin = {top: 20, right: 10, bottom: 10, left: 10};
     var width = 2000 - margin.right - margin.left;
     var height = 600 - margin.top - margin.bottom;
-
-
-    //this place the text x axis adjust this to center align the text
-    var tx = function (d) {
-        return d.x - 3;
-    };
-    //this place the text y axis adjust this to center align the text
-    var ty = function (d) {
-        return d.y + 3;
-    };
 
 
     var tree = d3.tree()
         .size([width, height]);
 
+    // Partners will be treated as children for graph (simplification):
     var root = d3.hierarchy(data,
         function children(d) {
             if (d["children"] != undefined)
@@ -24,6 +15,7 @@ function drawtree(data) {
             else return d.partners;
         });
 
+    // Create Plot Area and Group element:
     var svg = d3.select("#plotarea")
         .attr("width", width + margin.right + margin.left)
         .attr("height", width + margin.top + margin.bottom);
@@ -32,25 +24,30 @@ function drawtree(data) {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+    // Create Links
+    // note: every second layer will have different colour and line depth
+    // i.e., red=partnership/marriage, blue=parent/child relationship:
     var link = g.selectAll(".link")
         .data(tree(root).links())
-        .enter().append("path")
+        .enter()
+        .append("path")
         .attr("class", "link")
         .attr("d", function (link) {
-            return "M" + link.source.x + "," + link.source.y
-                + "L" + (link.source.x + link.target.x) / 2 + "," + link.source.y
-                + " " + link.target.x + "," + link.target.y;
+                return "M" + link.source.x + "," + link.source.y
+                    + "L" + (link.source.x + link.target.x) / 2 + "," + link.source.y
+                    + " " + link.target.x + "," + link.target.y;
             }
         )
         .attr("fill", "none")
-        .attr("stroke", function(link) {
+        .attr("stroke", function (link) {
             return (link.source.depth % 2 == 0) ? "red" : "blue";
         })
-        .attr("stroke-width", function(link) {
-            return (link.source.depth % 2 == 0) ? 4 : 2;
+        .attr("stroke-width", function (link) {
+            return (link.source.depth % 2 == 0) ? 3 : 3;
         });
 
 
+    // Create nodes:
     var node = g.selectAll(".node")
         .data(root.descendants())
         .enter()
@@ -62,18 +59,32 @@ function drawtree(data) {
             return "translate(" + node.x + "," + node.y + ")";
         });
 
+
+    // Create rectangles which will later be placed behind the names:
+    // note:x=-60 needed so that the rectangles will be placed in the center, not pushed to right from node
     node.append("rect")
         .attr("x", -60)
-        .attr("y", -15)
+        .attr("y", -20)
         .attr("width", 120)
         .attr("height", 30)
         .style("stroke", "black")
         .style("fill", "white");
 
+    // add the person's name to node:
     node.append("text")
         .attr('text-anchor', 'middle')
         .text(function (node) {
             return node.data.name;
         });
 
+    // add colours to legend table:
+    d3.select("table")
+        .selectAll("td")
+        .style("color",
+            function (d) {
+                if (this.innerText == "Husband/Wife")
+                    return "red";
+                else
+                    return "blue";
+            })
 }
