bb97af76ccab8593f28e3fc954fc318bb2ff8a1d
toludwig <toludwig@uos.de>
2017-12-17 16:10:00 +0100

    hand data not working yet

--- week4/two_panel.js
+++ week4/two_panel.js
@@ -1,79 +1,58 @@
-// WESENTLICHE ÄNDERUNG: PUNKTE ANZEIGEN LASSEN DURCH KLICK
-
 // DRAW POINTS PANEL:
 function drawPoints(data) {
 
     // Select area for points and define layout:
-    g = d3.select("#points");
+    svg = d3.select("#points");
 
     width = 800;
     height = 800;
     padding = 80;
 
-    // SCALING
-    // Fehler: die Punkte stimmen nicht mit den eigentlichen Ranges und Achsen überein:
-    // range of x-axis, i.e. data[0] = -0.4899838 to 0.6225203
-    // range of y-axis, i.e. data[1] = -0.4273515 to 0.4657887
-
-    // Print out min and max of first and second row's data values:
-    console.log(d3.extent(data, function(d){
-        return d[0];
-    }));
-
-    console.log(d3.extent(data, function(d){
-        return d[1];
-    }));
-
-
-
-
     // Scaling:
      var xScale = d3.scaleLinear()
         .domain(d3.extent(data, function(d){
-            return d[0];
+            return d.xy[0];
         }))
         .range([padding, width-padding]);
 
      var yScale = d3.scaleLinear()
         .domain(d3.extent(data, function(d){
-            return d[1];
+            return d.xy[1];
         }))
         .range([height-padding, padding]);
 
 
 
-
     // Create the data point circles:
-    var circles = g.selectAll("circle")
+    var circles = svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
         .attr("r", "5")
         .attr("cx", function (d) {
-            return ""+ yScale(d[0]) +"px";})
+            return ""+xScale(d.xy[0])+"px";
+        })
         .attr("cy", function (d) {
-            return ""+ yScale(d[1]) +"px";})
+            return ""+yScale(d.xy[1])+"px";
+        })
         .attr("fill", "lightblue")
         .attr("stroke", "black");
 
 
     // Check the values by clicking on the circles:
-
     circles.on("click", function (d) {
-            d3.select('#x_value')
-                .text(d[0]);
-            d3.select('#y_value')
-                .text(d[1]);});
-
-
+        d3.select('#id_value')
+            .text(d.id);
+        drawHand(d.id);
+    });
 
     // Include x-axis (bottom)
-    g.append('g')
+    svg.append('g')
         .attr('transform', 'translate(0,' + (height-padding) + ')')
         .attr("class","axis x")
         .call(d3.axisBottom(xScale));
 
-    g.append('g')
+    svg.append('g')
         .attr('transform', 'translate('+padding+', 0)')
         .attr("class","axis y")
         .call(d3.axisLeft(yScale));
@@ -83,5 +62,45 @@ function drawPoints(data) {
 
 
 // DRAW HAND PANEL:
-function drawHand(data) {
+function drawHand(id) {
+    // Select area for hand drawing
+    svg = d3.select("#hand");
+
+    width  = 800;
+    height = 800;
+    padding = 80;
+
+    var xScale = d3.scaleLinear()
+        .domain([0, 1.5])
+        .range([padding, width-padding]);
+
+    var yScale = d3.scaleLinear()
+        .domain([0, 1.5])
+        .range([height-padding, padding]);
+
+    function line(data) {
+        return d3.line()
+            .x(function (d) {
+                console.log(d);
+                return d.xs.map(function (x) {
+                    return xScale(x);
+                });
+            })
+            .y(function (d) {
+                return d.ys.map(function (y) {
+                    return yScale(y);
+                });
+            });
+    }
+
+    // Create the data point circles:
+    svg.append("path")
+        .datum(handData[id])
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 0.5)
+        .attr("d", line(handData[id]));
+
 }
