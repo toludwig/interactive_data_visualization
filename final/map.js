e0c2ee3289dee4994b4ada6a0147f85a9798a263
Mona <33896612+monatimmermann@users.noreply.github.com>
2018-01-10 18:49:07 +0100

    merged

--- final/map.js
+++ final/map.js
@@ -1,54 +1,77 @@
 function drawMap() {
-    MAP = L.map('map', {
-        zoomControl: false
-    }).setView([50,2], 4);
 
-    L.control.zoom({
-        position:"topright"
-    }).addTo(MAP);
+    // Center Coordinates (ca. Paris) and max. zoom:
+    home_lat=50;
+    home_long=2;
+    home_zoom=4;
+    max_zoom=19;
 
-    var home = {
-        lat: 50,
-        lng: 2,
-        zoom: 4
-    };
-    L.easyButton('<span>&starf;</span>',function(btn,map){
-        map.setView([home.lat, home.lng], home.zoom);
-    },'Zoom To Home')
+
+    // Initialise Map at predefined Center ("home"):
+    // "zoomControl: false" to avoid the default zoom buttons
+    var MAP= L.map('mapid', {zoomControl: false})
+        .setView([home_lat, home_long], home_zoom);
+
+    // Now add Zoom button in top-right corner:
+    L.control.zoom({position: "topright"}).addTo(MAP);
+
+    // Add Home Button to the map (leads you back to initial map layout):
+    L.easyButton('<span>&starf;</span>', function (map) {
+        map.setView([home.lat, home.lng], home.zoom);}, 'Zoom To Home')
         .addTo(MAP);
 
-    var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy;' +
-        '<a href="http://cartodb.com/attributions">CartoDB</a>',
-        subdomains: 'abcd',
-        maxZoom: 19,
-        minZoom: 4
-    }).addTo(MAP);
+
+    // Add tile layer to map and the source of the leaflet:
+    // Zoom is set to 4-19 so that Europe is displayed nicely.
+    // subdomains: "abcd" ??
+
+    var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+        {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy;' +
+        '<a href="http://cartodb.com/attributions">CartoDB</a>', maxZoom: max_zoom, minZoom: home_zoom})
+        .addTo(MAP);
+
+    //return MAP;
 }
 
 
 function drawPoints() {
-    /* Initialize the SVG layer */
+    // Initialize the SVG layer
     var svg = d3.select(MAP.getPanes().overlayPane).append("svg"),
         g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
+    // adapt Leaflet’s API to fit D3 by implementing a custom geometric transformation
+
+
     var circles = g.selectAll("circle")
         .data(data)
-        .enter().append("circle")
+        .enter()
+        .append("circle")
         .style("stroke", "black")
-        .style("opacity", .6)
+        .style("opacity", .8)
         .style("fill", "red")
+        .attr("x", function (d) {
+            LatLng = [d.latitude, d.longitude];
+            return map.latLngToLayerPoint(LatLng).x;
+        })
+        .attr("y", function (d) {
+            LatLng = [d.latitude, d.longitude];
+            return map.latLngToLayerPoint(LatLng).y;
+        })
         .attr("r", 5);
 
+    map.on("viewreset", update);
+
+    //update();
 
     function update() {
         console.log("in");
         circles.attr("transform",
-            function(d) {
+            function (d) {
                 LatLng = [d.latitude, d.longitude];
-                return "translate("+
-                    map.latLngToLayerPoint(LatLng).x +","+
-                    map.latLngToLayerPoint(LatLng).y +")";
+                // console.log(map.latLngToLayerPoint(LatLng));
+                return "translate(" +
+                    map.latLngToLayerPoint(LatLng).x + "," +
+                    map.latLngToLayerPoint(LatLng).y + ")";
             }
         );
 
@@ -58,9 +81,9 @@ function drawPoints() {
         console.log(xSpan);
         var margin = 50;
         svg.attr("width", xSpan[1]-xSpan[0]+margin)
-            .attr("height", ySpan[1]-ySpan[0]+margin)
-            .attr("left", xSpan[0] +"px")
-            .attr("top", ySpan[0] + "px");
+        .attr("height", ySpan[1]-ySpan[0]+margin)
+        .attr("left", xSpan[0] +"px")
+        .attr("top", ySpan[0] + "px");
     }
 
     MAP.on("viewreset", update);
