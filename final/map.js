2fe08ef9c1edaff7556e84ad30bafd324e454df3
toludwig <toludwig@uos.de>
2018-01-10 18:32:09 +0100

    bbox

--- final/map.js
+++ final/map.js
@@ -1,11 +1,11 @@
 function drawMap() {
-    var mymap = L.map('map', {
+    MAP = L.map('map', {
         zoomControl: false
     }).setView([50,2], 4);
 
     L.control.zoom({
         position:"topright"
-    }).addTo(mymap);
+    }).addTo(MAP);
 
     var home = {
         lat: 50,
@@ -15,7 +15,7 @@ function drawMap() {
     L.easyButton('<span>&starf;</span>',function(btn,map){
         map.setView([home.lat, home.lng], home.zoom);
     },'Zoom To Home')
-        .addTo(mymap);
+        .addTo(MAP);
 
     var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy;' +
@@ -23,15 +23,13 @@ function drawMap() {
         subdomains: 'abcd',
         maxZoom: 19,
         minZoom: 4
-    }).addTo(mymap);
-
-    return mymap;
+    }).addTo(MAP);
 }
 
 
-function drawPoints(map) {
+function drawPoints() {
     /* Initialize the SVG layer */
-    var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+    var svg = d3.select(MAP.getPanes().overlayPane).append("svg"),
         g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
     var circles = g.selectAll("circle")
@@ -40,28 +38,31 @@ function drawPoints(map) {
         .style("stroke", "black")
         .style("opacity", .6)
         .style("fill", "red")
-        .attr("x", function(d) {
-            LatLng = [d.latitude, d.longitude];
-            return map.latLngToLayerPoint(LatLng).x;
-        })
-        .attr("y", function(d) {
-            LatLng = [d.latitude, d.longitude];
-            return map.latLngToLayerPoint(LatLng).y;
-        })
-        .attr("r", 20);
+        .attr("r", 5);
 
-    map.on("viewreset", update);
-    //update();
 
     function update() {
+        console.log("in");
         circles.attr("transform",
             function(d) {
                 LatLng = [d.latitude, d.longitude];
-                console.log(map.latLngToLayerPoint(LatLng));
                 return "translate("+
                     map.latLngToLayerPoint(LatLng).x +","+
                     map.latLngToLayerPoint(LatLng).y +")";
             }
-        )
+        );
+
+        console.log(svg.node().getBBox());
+        var xSpan = d3.extent(circles.map(function (t) { return t.x; }));
+        var ySpan = d3.extent(circles.map(function (t) { return t.y; }));
+        console.log(xSpan);
+        var margin = 50;
+        svg.attr("width", xSpan[1]-xSpan[0]+margin)
+            .attr("height", ySpan[1]-ySpan[0]+margin)
+            .attr("left", xSpan[0] +"px")
+            .attr("top", ySpan[0] + "px");
     }
+
+    MAP.on("viewreset", update);
+    update();
 }
