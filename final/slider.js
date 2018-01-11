735f90c1b53488ddfce6ad92e36b6cec6356874f
JohannesJacob <johannes_jacob@gmx.de>
2018-01-11 15:15:44 +0100

    slider vorbereitet

--- final/slider.js
+++ final/slider.js
@@ -1,18 +1,22 @@
 // Here the source: https://codepen.io/2rod/pen/JtIki
 
+var lowerbound = new Date('2015.01.01');
+var upperbound = new Date('2015.12.30');
 $(function() {
     var slider = $( "#slider-range" ).slider({
       range: true,
-      min: new Date('2010.01.01').getTime() / 1000,
-      max: new Date('2014.01.01').getTime() / 1000,
+      min: new Date('2013.01.01').getTime() / 1000,
+      max: new Date('2017.01.01').getTime() / 1000,
       step: 86400,
-      values: [ new Date('2012.01.01').getTime() / 1000, new Date('2013.12.01').getTime() / 1000 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( (new Date(ui.values[ 0 ] *1000).toDateString() ) + " - " + (new Date(ui.values[ 1 ] *1000)).toDateString() );
+      values: [ new Date('2015.01.01').getTime() / 1000, new Date('2015.12.30').getTime() / 1000 ],
+      slide: function (event, ui){
+         lowerbound = (new Date(ui.values[ 0 ] *1000) );
+         upperbound = (new Date(ui.values[ 1 ] *1000) );
+         //drawPoints(); 
       }
+      
     });
-    $( "#amount" ).val( (new Date($( "#slider-range" ).slider( "values", 0 )*1000).toDateString()) +
-      " - " + (new Date($( "#slider-range" ).slider( "values", 1 )*1000)).toDateString());
+    $( "#coloredSlider .ui-slider-range" ).css( "background-color", myColor );
   });
 
 
